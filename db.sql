ecbefa8182c575977e7520a2aa93d7c17260eeb9
Yuda Cogati <yudapc@gmail.com>
2016-11-12 10:44:42 +0700

    initial commit

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+  "bytes"
+  "database/sql"
+  "fmt"
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+  _ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+  db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/go_sample")
+  if err != nil {
+    fmt.Print(err.Error())
+  }
+  defer db.Close()
+  // make sure connection is available
+  err = db.Ping()
+  if err != nil {
+    fmt.Print(err.Error())
+  }
+  type Users struct {
+    Id         int    `json:"id"`
+    First_Name string `json:"first_name"`
+    Last_Name  string `json:"last_name"`
+    Email      string `json:"email"`
+  }
+  router := gin.Default()
+
+  // GET a user detail
+  router.GET("/users/:id", func(c *gin.Context) {
+    var (
+      user Users
+      result gin.H
+    )
+    id := c.Param("id")
+    row := db.QueryRow("select id, first_name, last_name, email from users where id = ?;", id)
+    err = row.Scan(&user.Id, &user.First_Name, &user.Last_Name, &user.Email)
+    if err != nil {
+      // If no results send null
+      result = gin.H{
+        "result": nil,
+        "count":  0,
+      }
+    } else {
+      result = gin.H{
+        "result": user,
+      }
+    }
+    c.JSON(http.StatusOK, result)
+  })
+
+  // GET all users
+  router.GET("/users", func(c *gin.Context) {
+    var (
+      user  Users
+      users []Users
+    )
+    rows, err := db.Query("select id, first_name, last_name, email from users;")
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+    for rows.Next() {
+      err = rows.Scan(&user.Id, &user.First_Name, &user.Last_Name, &user.Email)
+      users = append(users, user)
+      if err != nil {
+        fmt.Print(err.Error())
+      }
+    }
+    defer rows.Close()
+    c.JSON(http.StatusOK, gin.H{
+      "result": users,
+      "count":  len(users),
+    })
+  })
+
+  // POST new user details
+  router.POST("/users", func(c *gin.Context) {
+    var buffer bytes.Buffer
+    first_name := c.PostForm("first_name")
+    last_name := c.PostForm("last_name")
+    email := c.PostForm("email")
+    // validate
+    // if first_name == "" || last_name == "" || email == "" {
+    //   // c.JSON(http.StatusUnprocessableEntity, gin.H{
+    //   //   "errors": "Invalid Parameters",
+    //   // })
+    //   // return
+    //   fmt.Print(err.Error())
+    // }
+    stmt, err := db.Prepare("insert into users (first_name, last_name, email) values(?,?,?);")
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+    _, err = stmt.Exec(first_name, last_name, email)
+
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+
+    // Fastest way to append strings
+    buffer.WriteString(first_name)
+    buffer.WriteString(" ")
+    buffer.WriteString(last_name)
+    defer stmt.Close()
+    name := buffer.String()
+    c.JSON(http.StatusOK, gin.H{
+      "message": fmt.Sprintf(" %s successfully created", name),
+    })
+  })
+
+  // PUT - update a user details
+  router.PUT("/users", func(c *gin.Context) {
+    var buffer bytes.Buffer
+    id := c.Query("id")
+    first_name := c.PostForm("first_name")
+    last_name := c.PostForm("last_name")
+    email := c.PostForm("email")
+    stmt, err := db.Prepare("update users set first_name= ?, last_name= ?, email= ? where id= ?;")
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+    _, err = stmt.Exec(first_name, last_name, email, id)
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+
+    // Fastest way to append strings
+    buffer.WriteString(first_name)
+    buffer.WriteString(" ")
+    buffer.WriteString(last_name)
+    defer stmt.Close()
+    name := buffer.String()
+    c.JSON(http.StatusOK, gin.H{
+      "message": fmt.Sprintf("Successfully updated to %s", name),
+    })
+  })
+
+  // Delete resources
+  router.DELETE("/users", func(c *gin.Context) {
+    id := c.Query("id")
+    stmt, err := db.Prepare("delete from users where id= ?;")
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+    _, err = stmt.Exec(id)
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+    c.JSON(http.StatusOK, gin.H{
+      "message": fmt.Sprintf("Successfully deleted users: %s", id),
+    })
+  })
+  router.Run(":3000")
+}
